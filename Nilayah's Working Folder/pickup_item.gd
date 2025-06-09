52831f5357d56463ade1695e8c14ac6c9d311848
Nilayah Peter <nilayahsp@gmail.com>
2025-06-09 23:18:09 +0200

    implement trash and recycling enums

--- Nilayah's Working Folder/pickup_item.gd
+++ Nilayah's Working Folder/pickup_item.gd
@@ -1,7 +1,21 @@
 extends Area2D
 
-var player: Player = null
 @onready var pickup_item: Area2D = $"."
+@onready var sprite: Sprite2D = $Sprite2D
+
+@export var item_type: ItemType = ItemType.TRASH
+
+# used chatgpt to learn how to use enums
+enum ItemType { TRASH, RECYCLABLE }
+
+var player: Player = null
+
+func _ready():
+	match item_type:
+		ItemType.TRASH:
+			sprite.region_rect = Rect2(205, 14, 37, 50)
+		ItemType.RECYCLABLE:
+			sprite.region_rect = Rect2(1, 15, 17, 49)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -15,4 +29,7 @@ func _process(delta: float) -> void:
 	if player and Input.is_action_just_pressed("pickup"):
 		if player.carrying_item == false:
 			queue_free()
-			player.pickup_item()
+			player.pickup_item(item_type)
+
+func _on_ready() -> void:
+	pass # Replace with function body.
