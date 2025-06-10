581a84776a449eea6ca027a1e1c374f6bda66d81
Nilayah Peter <nilayahsp@gmail.com>
2025-06-10 11:34:49 +0200

    integrate animatons

--- Nilayah's Working Folder/player.gd
+++ Nilayah's Working Folder/player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 class_name Player
 
-@export var SPEED = 100
-
+@onready var anim = load("res://Nilayah's Working Folder/zeke's_player_animations.tscn")
 @onready var pick_up = load("res://Nilayah's Working Folder/pickup_item.tscn")
 @onready var item_spr: Sprite2D = $ItemSprite
+
+@export var SPEED = 100
+@export var animation_tree : AnimationTree
 
 const PickupItem = preload("res://Nilayah's Working Folder/pickup_item.gd")
 
@@ -14,24 +16,42 @@ var current_item_type: PickupItem.ItemType = PickupItem.ItemType.TRASH
 var drop_pos := Vector2(0, 16)
 var items_in_range: Array = []
 
-func _on_ready() -> void:
+var input_direction : Vector2
+var playback : AnimationNodeStateMachinePlayback
+
+func _ready():
 	# Pick Up / Drop Item
 	item_spr.hide() 
+	
+	# Animation
+	playback = animation_tree["parameters/playback"]
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
-
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("pickup") and carrying_item == true:
-		drop_item()
-
+	select_animation()
+	update_animation_parameters()
+	
 # Movement
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 
-#Pick Up / Drop Item - used a youtube video to learn how to pick up and drop items
+# Animation
+func update_animation_parameters():
+	if input_direction == Vector2.ZERO:
+		return
+	
+	animation_tree["parameters/idle/blend_position"] = input_direction
+	animation_tree["parameters/walk/blend_position"] = input_direction
+	
+func select_animation():
+	if velocity == Vector2.ZERO:
+		playback.travel("idle")
+	else:
+		playback.travel("walk")
+		
+# Pick Up / Drop Item - used a youtube video to learn how to pick up and drop items
 func pickup_item(item_type):
 	carrying_item = true
 	current_item_type = item_type
