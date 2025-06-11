2ae9b37aaf864db278970e0e505e75771a42f155
Nilayah Peter <nilayahsp@gmail.com>
2025-06-11 21:11:42 +0200

    add more bins to level 1

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -4,16 +4,17 @@ extends Node2D
 @onready var end_timer: Timer = $EndTimer
 @onready var trash_bin: Area2D = $Bins/TrashBin
 @onready var recycling_bin: Area2D = $Bins/RecyclingBin
+@onready var trash_bin2: Area2D = $Bins/TrashBin2
+@onready var recycling_bin2: Area2D = $Bins/RecyclingBin2
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	player.can_move = true
 	if player.current_health == 0:
 		player.current_health -= 1
 		end_timer.start()
-	if trash_bin.items_in_bin_count + recycling_bin.items_in_bin_count == 13:
+	if trash_bin.items_in_bin_count + recycling_bin.items_in_bin_count + trash_bin2.items_in_bin_count + recycling_bin2.items_in_bin_count == 13:
 		trash_bin.items_in_bin_count += 1
 		end_timer.start()
 
