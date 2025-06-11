7b51c15d096ea270d0fd89afefc8f1346c81a534
Riley Little <rileylittle2211@gmail.com>
2025-06-11 15:16:32 +0200

    screens updated (good and bad)

--- scenes/ending_scene_script.gd
+++ scenes/ending_scene_script.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
-@onready var timer: Timer = $Timer
 
 
 func _ready():
@@ -9,3 +8,11 @@ func _ready():
 
 func _on_timer_timeout() -> void:
 	canvas_layer.visible = true
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
