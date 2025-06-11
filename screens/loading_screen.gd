69330a5e606ba4bbea11f7c9a348cdfff66b191c
Riley Little <rileylittle2211@gmail.com>
2025-06-11 13:59:31 +0200

    added timer and connected loading screens

--- screens/loading_screen.gd
+++ screens/loading_screen.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://rooms/room_3.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
