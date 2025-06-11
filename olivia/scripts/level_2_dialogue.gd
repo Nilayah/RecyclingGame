1f124b66be79c7162277a9c16918820407b1dfbf
olivialam24 <160654680+olivialam24@users.noreply.github.com>
2025-06-11 21:04:11 +0200

    Dialogue L1

--- olivia/scripts/level_2_dialogue.gd
+++ olivia/scripts/level_2_dialogue.gd
@@ -3,7 +3,9 @@ extends Node2D
 @onready var timer: Timer = $Timer
 @onready var house_mess: Label = $HouseMess
 @onready var clean: Label = $Clean
+@onready var find: Label = $Find
 @onready var hero: Label = $Hero
+@onready var white_background: Panel = $white_background
 
 var step: int = 0
 
@@ -11,7 +13,9 @@ var step: int = 0
 func _ready() -> void:
 	house_mess.visible = true
 	clean.visible = false
+	find.visible = false
 	hero.visible = false
+	white_background.visible = true
 	
 	timer.wait_time = 4
 	timer.start()
@@ -25,10 +29,16 @@ func _on_timer_timeout() -> void:
 			timer.start()
 		1:
 			clean.visible = false
+			find.visible = true
+			timer.wait_time = 5
+			timer.start()
+		2:
+			find.visible = false
 			hero.visible = true
 			timer.wait_time = 4
 			timer.start()
-		2:
+		3:
 			hero.visible = false
+			white_background.visible = false
 			timer.stop()  # All done
 	step += 1
